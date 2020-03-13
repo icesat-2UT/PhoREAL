44af85a0f72973b47869536e81be558bb643cc04
icesat-2UT <55703544+icesat-2UT@users.noreply.github.com>
2020-03-13 09:56:46 -0500

    Updates for assessing ATL03/ATL08
    Added a function to boost confidence of ground classifications and added a "perfect classifier"

---
+++ acer_code.pyx
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 12:56:37 2019
+
+@author: eguenther
+"""
+
+import numpy as np
+
+def aceReassign(indexc, indexg, zc, cc, zg):
+    print("ACE'ed")
+    for r in range(0,len(zg)):
+        if len(indexc[r]) == 0:
+            continue
+        cindex = np.array(indexc[r])
+        gindex = np.array(indexg[r])
+        zcl = zc[cindex]
+        ccl = cc[cindex]
+        if 9 in ccl:
+            continue
+        zdem = zg[gindex]
+        zdelta = zcl - zdem
+        check1 = ((zdelta <= 0.5) & (ccl == 1))
+        check2 = ((zdelta > 0.5) & (ccl == 1))
+        index1 = cindex[check1[:,0]]
+        index2 = cindex[check2[:,0]]
+        cc[index1] = 2
+        cc[index2] = 4
+    return cc
