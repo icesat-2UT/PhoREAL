Fixed the rebinning issue in binner.py because of incorrect values in the 'alongtrack' variable. Computes the shortest step between two segments (assuming adjacent), and corrects it to 100m. Fixes problem where adjacent segments were over 1.8km apart. I will have to look at my changes to utils.py before commenting on it, it's been a while.

phoreal/binner.py
@@ -567,7 +567,7 @@ def calculate_seg_meteric(df_in, df_out, classification, operation, field,
                outfield, key_field = 'bin_id', classfield = 'classification'):
     df_filter = df_in[df_in[classfield].isin(classification)].copy()
     df_filter.drop(df_filter.columns.difference([key_field,field]),
-                       1,inplace=True)
+                       axis=1,inplace=True)
     zgroup = df_filter.groupby(key_field)
     zout = zgroup.aggregate(operation)
     zout[key_field] = zout.index
@@ -583,7 +583,7 @@ def calculate_seg_percentile(df_in, df_out, classification, operation, field,
     q_list = [10,20,25,30,40,50,60,70,75,80,90,98,100]
     df_filter = df_in[df_in[classfield].isin(classification)].copy()
     df_filter.drop(df_filter.columns.difference([key_field,field]), 
-                   1,inplace=True)
+                   axis=1,inplace=True)
     zgroup = df_filter.groupby(key_field)
     zout = zgroup.aggregate(percentile_rh)
     zout[key_field] = zout.index
@@ -734,12 +734,17 @@ def rebin_atl08(atl03, atl08, gt, res, res_field):
     
     if res_field in ['delta_time','lat_ph','lon_ph','alongtrack','northing']: 
         at03 = np.array(atl03.df[res_field])
-        ind03 = np.int32(np.floor((at03 - np.min(at03))/res))
-        atl03.df['h_ind'] = ind03
-
-    #Place holder for ATL08
         at08 = np.array(atl08.df[res_field])
-        ind08 = np.int32(np.floor((at08 - np.min(at03))/res))
+        
+        at_step = min(abs(at08[i] - at08[i+1]) for i in range(len(at08) - 1))
+        
+        ind03 = np.int32(np.floor((at03 - np.min(at03))/at_step*(100/res)))
+        ind08 = np.int32(np.floor((at08 - np.min(at03))/at_step*(100/res)))
+        #ind03 = np.int32(np.floor((at03 - np.min(at03))/res))
+        #ind08 = np.int32(np.floor((at08 - np.min(at03))/res))
+        
+        
+        atl03.df['h_ind'] = ind03
         atl08.df['h_ind'] = ind08
         
     elif res_field == 'atl03_seg':
@@ -848,8 +853,8 @@ def rebin_atl08(atl03, atl08, gt, res, res_field):
     bin_df = calculate_seg_meteric(atl03.df, bin_df, [3], get_std, 'norm_h', 
                    'toc_roughness', key_field = 'h_ind', classfield = 'classification')
     #h_te_skew
-    bin_df = calculate_seg_meteric(atl03.df, bin_df, [1], get_skew, 'h_ph', 
-                   'h_te_skew', key_field = 'h_ind', classfield = 'classification')
+    #bin_df = calculate_seg_meteric(atl03.df, bin_df, [1], get_skew, 'h_ph', 
+    #f               'h_te_skew', key_field = 'h_ind', classfield = 'classification')
     #h_te_std
     bin_df = calculate_seg_meteric(atl03.df, bin_df, [1], get_std, 'h_ph', 
                    'h_te_std', key_field = 'h_ind', classfield = 'classification')
@@ -895,7 +900,7 @@ def rebin_atl08(atl03, atl08, gt, res, res_field):
     
     
     # Compute photon_rate_can
-    bin_df['photon_rate_can'] = (bin_df.n_ca_photons + bin_df.n_toc_photons) / bin_df.n_unique_shots
+    bin_df['photon_rate_can_nr'] = (bin_df.n_ca_photons + bin_df.n_toc_photons) / bin_df.n_unique_shots
     
     # Compute photon_rate_te
     bin_df['photon_rate_te'] = bin_df.n_te_photons / bin_df.n_unique_shots
@@ -1106,8 +1111,8 @@ def rebin_truth(atl03, truth_swath, res, res_field):
     bin_df = calculate_seg_meteric(truth_swath, bin_df, [4], get_len, 'h_ph', 
                    'n_ca_photons', key_field = 'h_ind', classfield = 'classification')
     #h_te_skew
-    bin_df = calculate_seg_meteric(truth_swath, bin_df, [2], get_skew, 'h_ph', 
-                   'h_te_skew', key_field = 'h_ind', classfield = 'classification')
+    #bin_df = calculate_seg_meteric(truth_swath, bin_df, [2], get_skew, 'h_ph', 
+    #               'h_te_skew', key_field = 'h_ind', classfield = 'classification')
     #h_te_std
     bin_df = calculate_seg_meteric(truth_swath, bin_df, [2], get_std, 'h_ph', 
                    'h_te_std', key_field = 'h_ind', classfield = 'classification')

phoreal/utils.py
@@ -518,8 +518,8 @@ def find_utm_zone_arr(lat, lon, mode=True, module=None, m=None):
             elif len(zone) > 3:
                 from scipy.stats import mode as mode_func
                 zone = mode_func(zone)[0]
-                print("Warning: Input ground track present in more than 3 UTM zones. \
-                         \nRecommend manually selecting GCS.")
+                #print("Warning: Input ground track present in more than 3 UTM zones. \
+                #         \nRecommend manually selecting GCS.")
             else:
                 # len(zone) == 0
                 print("Warning: zone == [], lon/lat may not have values")